66e586ca0ef7aae88a7736679710a440b86b05b4
Hilal Oruç <hiilaloruc@gmail.com>
2022-11-22 00:03:05 +0300

    Some class removals to get the most fundamental version

--- BankSystem/src/Main.java
+++ BankSystem/src/Main.java
@@ -8,7 +8,6 @@ public class Main {
     public static void main(String[] args) {
 
         Bank BNP = new Bank("BNP");
-
         Users Alex = BNP.CreateUser("Alex", 6220928, "contact@Alex.com");
         Users Hilal = BNP.CreateUser("Hilal", 6258741, "hilaloruc@gmail.com");
 
